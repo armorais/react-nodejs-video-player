583c43e3f8d86cb87bbbf986de56bd0a1d12ad39
Alison Morais <armorais01@gmail.com>
2021-05-27 13:42:23 -0300

    Moving the video player to a player component

--- react_app/src/components/Player/Player.test.js
+++ react_app/src/components/Player/Player.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Player from './Player';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<Player />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
